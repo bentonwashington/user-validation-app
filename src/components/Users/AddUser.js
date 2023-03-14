4182c8d9c92975bbf7f15f4e5022a976c9cb6091
Code 41 <washingtonbenton@gmail.com>
2023-03-14 19:54:12 -0400

    First commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AddUser(props) {
+    <form>
+        <label htmlFor="username" >Username</label>
+        <input id="username" type="text" ></input>
+
+        <label htmlFor="age" >Age (Years)</label>
+        <input id="age" type="number" ></input>
+    </form>
+
+
+}
+
+export default AddUser;
