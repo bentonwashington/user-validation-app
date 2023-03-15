758156a66b7616f57fe3725a31f39a7834470258
Code 41 <washingtonbenton@gmail.com>
2023-03-14 21:06:13 -0400

    Add reusable card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React from 'react';
+
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 function AddUser(props) {
     function addUserHandler(event) {
         event.preventDefault();
     };
 
-    return ( 
+
+    return (
+        <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
-        <label htmlFor="username" >Username</label>
-        <input id="username" type="text" ></input>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
 
-        <label htmlFor="age" >Age (Years)</label>
-        <input id="age" type="number" ></input>
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' />
 
-        <button type="submit">Add User</button>
+        <button type='submit'>Add User</button>
     </form>
-    );
+        </Card>
+
+)
 
 
-}
+
+};
 
 export default AddUser;
