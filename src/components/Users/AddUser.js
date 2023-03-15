839ba2f1d57a491ce7e6feca5cbc3dcc8e1206c1
Code 41 <washingtonbenton@gmail.com>
2023-03-14 20:00:14 -0400

    Add Usre Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 function AddUser(props) {
-    <form>
+    function addUserHandler(event) {
+        event.preventDefault();
+    };
+
+    return ( 
+    <form onSubmit={addUserHandler}>
         <label htmlFor="username" >Username</label>
         <input id="username" type="text" ></input>
 
         <label htmlFor="age" >Age (Years)</label>
         <input id="age" type="number" ></input>
+
+        <button type="submit">Add User</button>
     </form>
+    );
 
 
 }
